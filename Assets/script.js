53cd7dbb28f8a99f236a3507486267282a9ea2db
RaphaelMBarros <118280742+RaphaelMBarros@users.noreply.github.com>
2023-02-25 13:30:19 -0300

    Add arquivos da dinâmica site Lua

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+const modal = document.getElementById("modal");
+const cardModal = document.getElementById("cardModal");
+
+document.getElementById("lua").addEventListener("click", function(event){
+    event.preventDefault() 
+});
+
+function irParaLua () {    
+    modal.style.display = "";
+}
